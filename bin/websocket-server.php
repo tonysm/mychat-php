76eec23a6ae4d5a2f3f82b019d3895e3bbbe9711
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-06-12 23:19:55 -0300

    user can pass the port to the server

--- bin/websocket-server.php
+++ bin/websocket-server.php
@@ -1,11 +1,13 @@
 <?php
-
+// user can pass the port when starting the server
+$port = $argc == 2 ? $argv[1] : 8888;
+echo $port;
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
 use MyApp\WebsocketServer\Chat;
 
-$server = IoServer::factory(new WsServer(new Chat()), 8888);
+$server = IoServer::factory(new WsServer(new Chat()), $port);
 
 $server->run();
