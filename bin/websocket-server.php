618093d302fce1f1825e01e72f4a036b34ce6899
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-06-12 23:54:46 -0300

    remove echo

--- bin/websocket-server.php
+++ bin/websocket-server.php
@@ -1,7 +1,6 @@
 <?php
 // user can pass the port when starting the server
 $port = $argc == 2 ? $argv[1] : 8888;
-echo $port;
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Ratchet\Server\IoServer;
